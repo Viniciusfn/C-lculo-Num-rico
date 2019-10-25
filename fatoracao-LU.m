0ad3d6d1a18e3aa210796bd1fd1da800e0b13ba5
Vinícius Nóbrega <vinicius.nobrega@embedded.ufcg.edu.br>
2019-10-25 18:48:52 -0300

    Fatoracao LU implementada

---
+++ fatoracao-LU.m
@@ -0,0 +1,59 @@
+A = [21 -7 6; 3 12 5; -5 6 17];
+B = [19; 20; 21];
+
+t = size(A);
+if(t(1)!=t(2))
+  fprintf('Erro\n');
+endif
+
+tam = t(1);
+U = A;
+L = eye(tam);
+
+for posPivo = 1:1:tam
+  pivo = U(posPivo, posPivo);
+
+  if pivo == 0
+    fprintf('Erro\n');
+    break;
+  end
+
+  for lin = (posPivo+1):1:tam
+    mult = U(lin,posPivo)/pivo;
+	L(lin, posPivo) = mult;
+    for col = posPivo:1:tam
+      U(lin,col) = U(lin,col) - mult*(U(posPivo,col));
+    endfor
+  endfor
+endfor
+
+#L.Y = B
+Y = zeros(0,tam);
+
+for i = 1:1:tam
+	Y(i) = B(i,1);
+	for j = i-1:-1:1
+		Y(i) = Y(i) - (L(i,j)*Y(j));
+	endfor
+endfor
+
+fprintf("Matriz Y:\n");
+Y
+
+#U.X = Y
+X = zeros(0,tam);
+
+for i = tam:-1:1 
+  X(i) = Y(i);
+  for j = tam:-1:i+1
+    X(i) = X(i) - (U(i,j)*X(j));
+  endfor
+  X(i) = X(i)/U(i,i);
+endfor
+
+fprintf("Matriz X (solucao):\n");
+X
+
+fprintf("Matrizes L e U:\n");
+L
+U
